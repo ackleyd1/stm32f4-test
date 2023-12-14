ece6518a1d5615a8727be303f0c4d7bd2e186450
ackleyd1 <ackleyd1@msu.edu>
2023-12-14 15:16:09 -0500

    implement CS as GPIO, reads 0xFF75

--- src/main.c
+++ src/main.c
@@ -48,8 +48,9 @@ int main(void) {
 
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     // set SPI GPIOA pins to alternate mode
-    GPIOA->MODER |= (0x2 << 8);
-    GPIOA->PUPDR |= (0x2 << 8);
+    GPIOA->MODER |= (0x1 << 8);
+    GPIOA->PUPDR |= (0x1 << 8);
+    GPIOA->ODR |= (0x1 << 4);
 
     GPIOA->MODER |= (0x2 << 10);
     GPIOA->PUPDR |= (0x2 << 10);
@@ -61,11 +62,11 @@ int main(void) {
     GPIOA->PUPDR |= (0x2 << 14);
 
     // select SPI in AFRL
-    GPIOA->AFR[0] = 0x55550000;
+    GPIOA->AFR[0] = 0x55500000;
 
     // enable SPI interface on APB2
     RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-    SPI1->CR1 = (SPI_CR1_DFF | SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_BR_1 | SPI_CR1_BR_0 | SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA);
+    SPI1->CR1 = (SPI_CR1_DFF | SPI_CR1_SSM | SPI_CR1_BR_1 | SPI_CR1_BR_0 | SPI_CR1_MSTR);
     SPI1->CR2 |= SPI_CR2_SSOE;
 
     // set reset pin low
@@ -82,36 +83,22 @@ int main(void) {
     uint16_t cmd_write = (RA8875_CMDWRITE << 8);
     uint16_t data_read = (RA8875_DATAREAD << 8);
 
-    SPI1->CR1 &= ~(0x1 << 8);
+    GPIOA->ODR &= ~(0x1 << 4);
     while(!(SPI1->SR & SPI_SR_TXE)){}
     SPI1->DR = cmd_write;
     while(!(SPI1->SR & SPI_SR_TXE)){}
     while(!(SPI1->SR & SPI_SR_RXNE)){}
     uint16_t data = SPI1->DR;
-    SPI1->CR1 |= (0x1 << 8);
+    GPIOA->ODR |= (0x1 << 4);
 
-    SPI1->CR1 &= ~(0x1 << 8);
+    GPIOA->ODR &= ~(0x1 << 4);
     SPI1->DR = data_read;
     while(!(SPI1->SR & SPI_SR_TXE)){}
     while(!(SPI1->SR & SPI_SR_RXNE)){}
     data = SPI1->DR;
-    SPI1->CR1 |= (0x1 << 8);
+    GPIOA->ODR |= (0x1 << 4);
 
-    // real read
-    SPI1->CR1 &= ~(0x1 << 8);
-    SPI1->DR = cmd_write;
-    while(!(SPI1->SR & SPI_SR_TXE)){}
-    while(!(SPI1->SR & SPI_SR_RXNE)){}
-    data = SPI1->DR;
-    SPI1->CR1 |= (0x1 << 8);
-
-    SPI1->CR1 &= ~(0x1 << 8);
-    SPI1->DR = data_read;
-    while(!(SPI1->SR & SPI_SR_TXE)){}
-    while(!(SPI1->SR & SPI_SR_RXNE)){}
-    data = SPI1->DR;
-    SPI1->CR1 |= (0x1 << 8);
-
+    
     // set LD1 on
     led_on = 0x0;
     while (1) {
